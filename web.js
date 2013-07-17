65b8690e1c7d05f95fc193332bbfcbf100560909
jeffrey-l-turner <jeffrey.l.turner@me.com>
2013-07-17 22:37:57 +0000

    edits on faq and web.js to cache index

--- web.js
+++ web.js
@@ -6,25 +6,9 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var size = fs.statSync("index.html").size;
-    var buffer = new Buffer(size);
-    buffer = fs.readFileSync("index.html");
-    response.send(buffer.toString("ascii", 0, size-1));
+    response.send(indexbuffer.toString("ascii", 0, indexsize-1));
 });
 
-/* // serve static file requests -- this must be placed last on app.get list 
-app.get(/^(.+)$/, function(request, response){ 
-    console.log('static file request : ' + request.params);
-    var path = [request.params.size];
-    path = 
-    console.log("Calling stat with request.params = " + request.params + " path " + path);
-    var size = fs.statSync(path).size;
-    var buffer = new Buffer(size);
-    console.log("Called fs.readFileSync with, buffer = " + buffer + "request = " + request);
-//    buffer = fs.readFileSync(request.toString);
-//    response.send(buffer.toString("utf-8", 0, size-1));
- });
-*/
 
 
 /* serve static image png file requests -- placing this near end of app.get list */
@@ -41,13 +25,18 @@ app.get(/^(.+)$/, function(request, response){
     res.sendfile(path, {root: __dirname});
 //    res.send(buffer);
  });
+<-- May add this back in later to serve specific static files based on type; used app.use(express.static(...)) to serve files out of static assets directory; see below
 */
 
 // {            'Content-Type': meta.contentType}
 
 var port = process.env.PORT || 8080;
-app.use(express.favicon('./favicon.ico'));
+app.use(express.favicon(__dirname + '/assets/favicon.ico'));
 app.use(express.static(__dirname + '/assets'));
+// Speedup by serving index file out of buffer by setting some global variables for app.get to use
+var indexsize = fs.statSync("index.html").size;
+var indexbuffer = new Buffer(indexsize);
+indexbuffer = fs.readFileSync("index.html");
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
